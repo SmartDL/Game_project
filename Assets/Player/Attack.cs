fae3a872a935427a8c9e825ab1fa405ca7aa8c07
Lang Dong <30867414+SmartDL@users.noreply.github.com>
2022-06-07 07:00:14 +0100

    Add files via upload

---
+++ Assets/Player/Attack.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject HammorObject;
+    public GameObject SwordObject;
+    public bool Hammor_get = false;//if get the hammor?
+    bool Hammor_On = false;
+    bool Sword_On = true;
+    WieldHammor Hammor;
+    WieldSword Sword;
+    public int Pattack = 2;
+    float Timer = 0;
+    GameText gameText;
+    public bool finish_get = false;//used to delete the text
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hammor = HammorObject.GetComponent<WieldHammor>();
+        Sword = SwordObject.GetComponent<WieldSword>();
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        gameText = gameControllerObject.GetComponent<GameText>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z) && Hammor_On)
+        {
+            Sword_On = true;
+            SwordObject.SetActive(true);
+            Hammor_On = false;
+            HammorObject.SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            if (Hammor_get && Sword_On)
+            {
+                Sword_On = false;
+                SwordObject.SetActive(false);
+                Hammor_On = true;
+                HammorObject.SetActive(true);
+            }
+        }
+        if (Timer > 0.3)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && Hammor_On)
+            {
+                Hammor.Wield();
+                Timer = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.F) && Sword_On)
+            {
+                Sword.Wield(Pattack);
+                Timer = 0;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.E) && Hammor_On)
+            Hammor.Wieldback();
+        if (Input.GetKeyUp(KeyCode.F) && Sword_On)
+            Sword.Wieldback();
+        else
+            Timer += Time.deltaTime;
+        //Debug.Log(Timer);
+        if (finish_get)
+            if (Input.GetKeyUp(KeyCode.N))//Only the first time to do this
+            {
+                gameText.ChangeText("");
+                finish_get = false;
+            }
+    }
+}
---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float verticalVelocity = 0;
+    public int MoSpeed = 5;
+    public int RoSpeed = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        // rotate the player object about the Y axis
+        // based on the mouse moving left and right
+        float rotation = Input.GetAxis("Mouse X");
+        transform.Rotate(0, RoSpeed * rotation, 0);
+        // rotate the camera (the player's "head") about its X axis
+        // based on the mouse moving up and down
+        float updown = Input.GetAxis("Mouse Y");
+        Camera.main.transform.Rotate(-(RoSpeed * updown), 0, 0);
+
+        // moving the player object forwards and backwards
+        float forwardSpeed = Input.GetAxis("Vertical");
+        // moving left to right
+        float lateralSpeed = Input.GetAxis("Horizontal");
+
+        // apply gravity – or rather figure out the resultant velocity
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+        CharacterController characterController = GetComponent<CharacterController>();
+
+        if (Input.GetButton("Jump") && characterController.isGrounded)
+        {
+            verticalVelocity = 5;
+        }
+        Vector3 speed = new Vector3(MoSpeed * lateralSpeed, verticalVelocity, MoSpeed * forwardSpeed);
+        // transform this absolute speed relative to the player's current rotation
+        // i.e. we don't want them to move "north", but forwards depending on where
+        // they are facing
+        speed = transform.rotation * speed;
+        // what is deltaTime?
+        // move at a different speed to make up for variable framerates
+        characterController.Move(speed * Time.deltaTime);
+    }
+}
