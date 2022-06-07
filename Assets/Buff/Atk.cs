5bf0b57d35a5faf2ed1fa236c18019fc090328e2
Lang Dong <30867414+SmartDL@users.noreply.github.com>
2022-06-07 06:52:08 +0100

    Add files via upload

---
+++ Assets/Buff/Atk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Attack>().Pattack += 2; 
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Buff/HP.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP : MonoBehaviour
+{
+    public GameObject GameController;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+            GameStart gamestart = GameController.GetComponent<GameStart>();
+            gamestart.PlayerHP += 5;
+            
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
